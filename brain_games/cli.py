d2153373f5275169a6e90ee030747def3bae1d29
Zakhar Chaichenko <chaichenko.98@mail.ru>
2022-05-02 12:00:40 +0300

    add .

---
+++ brain_games/cli.py
@@ -0,0 +1,4 @@
+import prompt
+def welcome_user():
+    name = prompt.string('Введи своё имя ')
+    print(f'Привет, {name}')
--- brain_games/scripts/cli.py
+++
@@ -1,4 +0,0 @@
-from prompt
-def welcome_user(name):
-    name = prompt.name('Скажи своё имя, браток? ')
-    print(f'Привет, {name})
