b569b1939d203c4e3375076f9f6b8fd59ef83557
Zakhar Chaichenko <chaichenko.98@mail.ru>
2022-06-26 16:04:14 +0300

    add .

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -9,12 +9,13 @@ END_A = 50
 START_X = 1
 END_X = 6
 
+NUMBERS_IN_PROGRESSION = 9
 
 def question_and_answer():
     progression = []
     start = randint(START_A, END_A)
     step = randint(START_X, END_X)
-    end = start + 9 * step + 1
+    end = start + NUMBERS_IN_PROGRESSION * step + 1
 
     for i in range(start, end, step):
         progression.append(str(i))
